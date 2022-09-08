dbb60dd6b224d856c1d8d5e946e47325b6820d25
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-09-08 10:50:32 +0200

    xr

---
+++ Assets/Scripts/Disc Mechanics/DragObject.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+
+    private float mZCoord;
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("On Mouse Down");
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        
+        // z  voordinate of game object screen 
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+}
