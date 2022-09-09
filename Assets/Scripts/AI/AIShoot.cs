48c2ac1ab979983581a7fdca4074c787c9eb4d98
jkalnins81 <j_kalnins@hotmail.com>
2022-09-09 10:13:24 +0200

    AI laser

--- Assets/Scripts/AI/AIShoot.cs
+++ Assets/Scripts/AI/AIShoot.cs
@@ -7,6 +7,11 @@ public class AIShoot : MonoBehaviour
     public int delayBetweenShots;
     public GameObject laserPrefab;
 
-    
+    private void Start()
+    {
+        Instantiate(laserPrefab, transform.position, Quaternion.identity);
+    }
+
+
     // Shoot in a random place within a funnel towards the player
 }
---
+++ Assets/Scripts/AI/Laser.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public int speed;
+    GameObject AIEnemy;
+
+    void Start()
+    {
+        AIEnemy = GameObject.FindGameObjectWithTag("AIEnemy");
+
+        this.GetComponent<Rigidbody>().velocity = AIEnemy.transform.forward * speed;
+    }
+}
