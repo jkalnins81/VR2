6a6dcadaa89bd485ab57dab5dfb9f20fae70bd51
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-09-12 15:23:18 +0200

    orientation on hands

--- Assets/Scripts/HandPrecense.cs
+++ Assets/Scripts/HandPrecense.cs
@@ -9,50 +9,28 @@ using XRController = UnityEngine.XR.Interaction.Toolkit.XRController;
 
 public class HandPrecense : MonoBehaviour
 {
-    [SerializeField] private  XRController controller;
+    public Transform target;
+    private Rigidbody _rb;
     
     void Start()
     {
-        // List<InputDevice> devices = new List<InputDevice>();
-        // InputDeviceCharacteristics rightControllerCharacteristics = 
-        //     InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller; 
-        //
-        // InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
-        //
-        // foreach (var item  in devices)
-        // {
-        //     Debug.Log("hi");
-        //     Debug.Log(item.name + item.characteristics);
-        // }
-        //
-        // if (devices.Count > 0)
-        // {
-        //     _targetDevice = devices[0];
-        // }
-        // else Debug.Log("No devises found");
-
-        // Debug.Log(devices);
+        _rb = GetComponent<Rigidbody>();
     }
     
-    void Update()
+    void FixedUpdate()
     {
-        controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool isPressed);
-        Debug.Log(isPressed);
-       
-        // controller.inputDevice.(CommonUsages.triggerButton, out bool primaryBottonValue);
-        //     if (primaryBottonValue)
-        //         Debug.Log("Pressing Primary Button");
-        //
-        //
-        // _targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-        //     if (triggerValue > 0.1f)
-        //         Debug.Log("Trigger pressed " + triggerValue);
-        //
-        // _targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-        //     if (primary2DAxisValue != Vector2.zero)
-        //         Debug.Log("Primary Touchpad " + primary2DAxisValue);
-        //     
-            
-            
+        // Pos
+        _rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        
+        // Rot
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+
+        _rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
+
+
+
     }
 }
