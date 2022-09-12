5d78c7d353ed4ae32e360a301fae774d8b043db2
jkalnins81 <j_kalnins@hotmail.com>
2022-09-12 10:30:26 +0200

    AIHealth script && AIDie function

---
+++ Assets/Scripts/AI/AIHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIHealth : MonoBehaviour
+{
+    public int AIHealthPoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FrontWall"))
+        {
+            AIDie();
+        }
+    }
+
+    void AIDie()
+    {
+        // explosion && visual effect
+        Destroy(transform.parent.gameObject);
+    }
+}
