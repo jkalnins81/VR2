783d4014d016e358400a58f9382530933634295b
jkalnins81 <j_kalnins@hotmail.com>
2022-09-12 20:30:39 +0200

    Updates

--- Assets/Scripts/ShieldEffect/ShieldRipples.cs
+++ Assets/Scripts/ShieldEffect/ShieldRipples.cs
@@ -8,8 +8,19 @@ public class ShieldRipples : MonoBehaviour
 
     private Material mat;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        //if(other.)
+        if (collision.gameObject.CompareTag("AILaser"))
+        {
+            var ripples = Instantiate(ripplesVFX, transform) as GameObject;
+            var psr = ripples.transform.GetChild(0).GetComponent<ParticleSystemRenderer>();
+            mat = psr.material;
+            
+            mat.SetVector("_SphereCenter", collision.contacts[0].point);
+
+            // this is the same as a bulletHole?
+
+            Destroy(ripples, 2);
+        }
     }
 }
