6098dc8b1d2c552361a56a86989d9b9e76b2fe47
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-09-14 10:47:09 +0200

    Disc release behaviour

--- Assets/Scripts/Disc/DiscBehaviourRelease.cs
+++ Assets/Scripts/Disc/DiscBehaviourRelease.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class DiscBehaviourRelease : MonoBehaviour
 {
@@ -10,8 +11,10 @@ public class DiscBehaviourRelease : MonoBehaviour
 
     public Collider collider;
 
-    private float discMaxSpeed = 1;
-    private float forceMultiplier = 10;
+    private float controllerMaxSpeed = 0.5f;
+    private float forceMultiplier = 100;
+
+    public XRGrabInteractable thisXRGrabInteractable;
     
     private void Start()
     {
@@ -19,33 +22,44 @@ public class DiscBehaviourRelease : MonoBehaviour
         controllerVelocity = FindObjectOfType<ControllerVelocity>();
 
         collider = GetComponent<Collider>();
+        thisXRGrabInteractable = GetComponent<XRGrabInteractable>();
     }
 
     public void ReleaseVelocity()
     {
         float handSpeed = controllerVelocity.rightHandRB.velocity.magnitude;
-
-        if(handSpeed > discMaxSpeed)
+        
+        if(handSpeed >= controllerMaxSpeed)
         {
-            Debug.Log(discMaxSpeed);
-            discRB.AddForce(controllerVelocity.rightHandRB.velocity.normalized  * forceMultiplier, ForceMode.Impulse);
+            Debug.Log("Using max Speed");
+
+            //Cap the max throw speed vel
+            //Start coroutine and add force after the XR scripts adds it's own throw velocity 
+            StartCoroutine(AddForceAfterThrow());
         }
         else
         {
-            float discPercentage = handSpeed / discMaxSpeed;
-            discRB.AddForce(controllerVelocity.rightHandRB.velocity.normalized * discPercentage * forceMultiplier, ForceMode.Impulse);
+            Debug.Log("Using Low speed");
+            // if arm movement is slow, output this 
+            // float discPercentage = handSpeed / discMaxSpeed;
+            // Debug.Log(discPercentage);
+            thisXRGrabInteractable.throwVelocityScale = 1.0f;
+            discRB.AddForce(controllerVelocity.rightHandRB.velocity.normalized, ForceMode.Impulse);
             
-            Debug.Log(discPercentage);
+  
         }
+
     }
 
     public void DisableDiscCollider()
     {
+        //Disable collider from controller script 
         collider.enabled = false; 
     }
     
     public void EnabaleDiscCollider()
     {
+        //Enable collider from controller script 
         StartCoroutine(DiscColliderOn());
     }
 
@@ -55,4 +69,14 @@ public class DiscBehaviourRelease : MonoBehaviour
         collider.enabled = true; 
     }
 
+
+    IEnumerator AddForceAfterThrow()
+    {
+        //Ads force to the disc once it is released and has got the velocity from the controller 
+        yield return new WaitForSeconds(0.01f);
+        thisXRGrabInteractable.throwVelocityScale = 50f;
+        thisXRGrabInteractable.throwAngularVelocityScale = 10.0f;
+        discRB.AddForce(controllerVelocity.rightHandRB.velocity.normalized  * forceMultiplier, ForceMode.Impulse);
+        
+    }
 }
