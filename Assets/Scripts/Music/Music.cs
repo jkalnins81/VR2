859c3eab9b00962fd81ce1dccabbbe3966fc2404
jkalnins81 <j_kalnins@hotmail.com>
2022-09-15 10:36:48 +0200

    DiscoDisc Music added

---
+++ Assets/Scripts/Music/Music.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource discoDiscMusic;
+
+    void Start()
+    {
+        discoDiscMusic.Play();
+    }
+}
