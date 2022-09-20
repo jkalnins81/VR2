dad15311eab6ccabd8432201092548a2a9b6c9cc
jkalnins81 <j_kalnins@hotmail.com>
2022-09-20 09:36:24 +0200

    Snake progress

--- Assets/Scripts/AI/Boss/Snake/SnakeManager.cs
+++ Assets/Scripts/AI/Boss/Snake/SnakeManager.cs
@@ -4,31 +4,70 @@ using UnityEngine;
 
 public class SnakeManager : MonoBehaviour
 {
-    [SerializeField] float speed = 0.25f;
+    [SerializeField] float distanceBetween = 0.2f;
+    [SerializeField] float speed = 100f;
     [SerializeField] float turnSpeed = 2f;
     [SerializeField] List<GameObject> bodyParts = new List<GameObject>();
     List<GameObject> snakeBody = new List<GameObject>();
 
+    float countUp = 0f;
+
     private void Start()
     {
-        GameObject temp = Instantiate(bodyParts[0], transform.position, transform.rotation, transform);
-        if (!temp.GetComponent<MarkerManager>())
-            temp.AddComponent<MarkerManager>();
-        if(!temp.GetComponent<Rigidbody>())
-        {
-            temp.AddComponent<Rigidbody>();
-            temp.GetComponent<Rigidbody>().useGravity = false;
-        }
-        snakeBody.Add(temp);
+        countUp = distanceBetween;
+        CreateBodyParts();
+
     }
 
     private void FixedUpdate()
     {
+        if(bodyParts.Count > 0)
+        {
+            CreateBodyParts();
+        }
         SnakeMovement();
     }
 
     void SnakeMovement()
     {
         snakeBody[0].GetComponent<Rigidbody>().velocity = snakeBody[0].transform.forward * speed * Time.deltaTime;
+    }
+
+    void CreateBodyParts()
+    {
+        if(snakeBody.Count == 0)
+        {
+            GameObject temp1 = Instantiate(bodyParts[0], transform.position, transform.rotation, transform);
+            if (!temp1.GetComponent<MarkerManager>())
+                temp1.AddComponent<MarkerManager>();
+            if (!temp1.GetComponent<Rigidbody>())
+            {
+                temp1.AddComponent<Rigidbody>();
+                temp1.GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+
+        MarkerManager markM = snakeBody[snakeBody.Count - 1].GetComponent<MarkerManager>();
+        if(countUp == 0)
+        {
+            markM.ClearMarkerList();
+        }
+        countUp += Time.deltaTime;
+        if(countUp >= distanceBetween)
+        {
+            GameObject temp = Instantiate(bodyParts[0], markM.markerList[0].position, markM.markerList[0].rotation, transform);
+            if (!temp.GetComponent<MarkerManager>())
+                temp.AddComponent<MarkerManager>();
+            if(!temp.GetComponent<Rigidbody>())
+            {
+                temp.AddComponent<Rigidbody>();
+                temp.GetComponent<Rigidbody>().useGravity = false;
+            }
+
+            snakeBody.Add(temp);
+            bodyParts.RemoveAt(0);
+            temp.GetComponent<MarkerManager>().ClearMarkerList();
+            countUp = 0;
+        }
     }
 }
