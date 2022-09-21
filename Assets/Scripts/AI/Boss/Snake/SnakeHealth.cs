96fbdaf775545a3a29ea48278d308443f8c89ed9
jkalnins81 <j_kalnins@hotmail.com>
2022-09-21 15:26:29 +0200

    Snake dies in sections rather than individually

--- Assets/Scripts/AI/Boss/Snake/SnakeHealth.cs
+++ Assets/Scripts/AI/Boss/Snake/SnakeHealth.cs
@@ -8,6 +8,18 @@ public class SnakeHealth : MonoBehaviour
     public int SnakeHeadHealthPoints;
     public GameObject AIExplosion;
 
+    public GameObject snake1;
+    public GameObject snake2;
+    public GameObject snake3;
+    public GameObject snake4;
+    public GameObject snake5;
+    public GameObject snake6;
+    public GameObject snake7;
+    public GameObject snake8;
+    public GameObject snake9;
+    public GameObject snake10;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(this.gameObject.CompareTag("SnakeHead"))
@@ -21,6 +33,101 @@ public class SnakeHealth : MonoBehaviour
                 {
                     Instantiate(AIExplosion, transform.position, Quaternion.identity);
                     SnakeDie();
+                }
+
+                if(SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake1"))
+                {
+                    Destroy(snake1);
+                    Destroy(snake2);
+                    Destroy(snake3);
+                    Destroy(snake4);
+                    Destroy(snake5);
+                    Destroy(snake6);
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake2"))
+                {
+                    Destroy(snake2);
+                    Destroy(snake3);
+                    Destroy(snake4);
+                    Destroy(snake5);
+                    Destroy(snake6);
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake3"))
+                {
+                    Destroy(snake3);
+                    Destroy(snake4);
+                    Destroy(snake5);
+                    Destroy(snake6);
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake4"))
+                {
+                    Destroy(snake4);
+                    Destroy(snake5);
+                    Destroy(snake6);
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake5"))
+                {
+                    Destroy(snake5);
+                    Destroy(snake6);
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake6"))
+                {
+                    Destroy(snake6);
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake7"))
+                {
+                    Destroy(snake7);
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake8"))
+                {
+                    Destroy(snake8);
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake9"))
+                {
+                    Destroy(snake9);
+                    Destroy(snake10);
+                }
+
+                if (SnakeHealthPoints <= 0 && this.gameObject.CompareTag("Snake10"))
+                {
+                    Destroy(snake10);
                 }
             }
         }
