44f420fd17faf7baf711256b6440c172d6ca90a7
RicheyDev <iamrohr@hotmail.com>
2022-10-04 15:26:33 +0200

    Balancing fireworks

--- Assets/Fireworks.cs
+++ Assets/Fireworks.cs
@@ -24,7 +24,11 @@ public class Fireworks : MonoBehaviour
     IEnumerator PlayFireworks()
     {
         _audioSource.PlayOneShot(fireworksClip, 2);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.1f);
+        _audioSource.PlayOneShot(fireworksClip, 2);
+        yield return new WaitForSeconds(0.2f);
+        _audioSource.PlayOneShot(fireworksClip, 2);
+        yield return new WaitForSeconds(0.1f);
         _audioSource.PlayOneShot(fireworksClip, 2);
         yield return new WaitForSeconds(0.5f);
         _audioSource.PlayOneShot(fireworksClip, 2);
@@ -32,17 +36,35 @@ public class Fireworks : MonoBehaviour
         _audioSource.PlayOneShot(fireworksClip, 2);
         yield return new WaitForSeconds(0.1f);
         fireWorks[0].SetActive(true);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.2f);
         fireWorks[1].SetActive(true);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.2f);
         fireWorks[2].SetActive(true);
-        yield return new WaitForSeconds(1f);
+        _audioSource.PlayOneShot(fireworksClip, 2);
+        yield return new WaitForSeconds(0.2f);
         fireWorks[3].SetActive(true);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(0.2f);
+        fireWorks[4].SetActive(true);
+        _audioSource.PlayOneShot(fireworksClip, 2);
+        yield return new WaitForSeconds(0.2f);
+        fireWorks[5].SetActive(true);
+     
+        yield return new WaitForSeconds(0.2f);
+        fireWorks[6].SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        _audioSource.PlayOneShot(fireworksClip, 2);
+        fireWorks[7].SetActive(true);
+        yield return new WaitForSeconds(1f);
+        fireWorks[8].SetActive(true);
+        yield return new WaitForSeconds(0.2f);
         fireWorks[0].SetActive(false);
         fireWorks[1].SetActive(false);
         fireWorks[2].SetActive(false);
         fireWorks[3].SetActive(false);
+        fireWorks[4].SetActive(false);
+        fireWorks[5].SetActive(false);
+        fireWorks[6].SetActive(false);
+        fireWorks[7].SetActive(false);
         
 
     }
