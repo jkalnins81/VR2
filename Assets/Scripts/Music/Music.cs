19f3ebe679d6474babea74fc88c057d9838172a8
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-10-06 15:02:24 +0200

    publish last

--- Assets/Scripts/Music/Music.cs
+++ Assets/Scripts/Music/Music.cs
@@ -6,7 +6,12 @@ public class Music : MonoBehaviour
 {
     public AudioSource discoDiscMusic;
 
-    public GameObject fireworksStart;
+    public GameObject fireworksStart1;
+    public GameObject fireworksStart2;
+    public GameObject fireworksStart3;
+    public GameObject fireworksStart4;
+    public GameObject fireworksStart5;
+    public GameObject fireworksStart6;
 
     void Start()
     {
@@ -17,7 +22,23 @@ public class Music : MonoBehaviour
     {
         yield return new WaitForSeconds(6.4f);
         discoDiscMusic.Play();
-        fireworksStart.SetActive(true);
-        Destroy(fireworksStart.transform.parent, 3f);
+        yield return new WaitForSeconds(0.2f);
+        fireworksStart1.SetActive(true);
+        Destroy(fireworksStart1.transform.parent, 3f);
+        yield return new WaitForSeconds(0.2f);
+        fireworksStart2.SetActive(true);
+        Destroy(fireworksStart1.transform.parent, 3f);
+        yield return new WaitForSeconds(0.2f);
+        fireworksStart3.SetActive(true);
+        Destroy(fireworksStart1.transform.parent, 3f);
+        yield return new WaitForSeconds(0.2f);
+        fireworksStart4.SetActive(true);
+        Destroy(fireworksStart1.transform.parent, 3f);
+        yield return new WaitForSeconds(0.2f);
+        fireworksStart5.SetActive(true);
+        Destroy(fireworksStart1.transform.parent, 3f);
+        fireworksStart6.SetActive(true);
+        Destroy(fireworksStart1.transform.parent, 3f);
+
     }
 }
